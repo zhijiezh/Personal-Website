cfa983d9819bfd95178d34d16476e336d6b3b1d0
zhijiezh <jerry_shh@hotmail.com>
2021-12-11 21:29:35 -0500

    finish part of the navigation bar

---
+++ components/logo.js
@@ -0,0 +1,44 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import { Text, useColorModeValue } from '@chakra-ui/react'
+import styled from "@emotion/styled"
+
+
+const LogoBox = styled.span`
+  font-weight: bold;
+  font-size: 18px;
+  display: inline-flex;
+  align-items: center;
+  height: 30px;
+  line-height: 20px;
+  padding: 10px;
+
+  &:hover img {
+    transform: rotate(20deg);
+  }
+`
+
+const Logo = () => {
+  // TODO: Make a logo for myself
+  // const footPrintImg = `/images/footprint${useColorModeValue('', '-dark')}.png`
+
+  return (
+    <Link href="/">
+      <a>
+        <LogoBox>
+          {/* <Image src={footPrintImg} width={20} height={20} alt="logo" />   */}
+          <Text
+            color={useColorModeValue('gray.800', 'whiteAlpha.900')}
+            fontFamily="M PLUS Rounded 1c"
+            fontWeight="bold"
+            ml={3}
+          >
+            Zhijie Zhao
+          </Text>
+        </LogoBox>
+      </a>
+    </Link>
+  )
+}
+
+export default Logo
