7976e046334d66868cf4a0577dca0455f4e7ce10
zhijiezh <jerry_shh@hotmail.com>
2021-12-13 15:27:37 -0500

    Add hamburger list

--- components/navbar.js
+++ components/navbar.js
@@ -11,8 +11,10 @@ import {
   MenuItem,
   MenuButton,
   IconButton,
-  useColorModeValue
+  useColorModeValue,
+  MenuList
 } from '@chakra-ui/react'
+import { HamburgerIcon } from '@chakra-ui/icons'
 
 const LinkItem = ({ href, path, children }) => {
   const active = path === href
@@ -55,6 +57,45 @@ const Navbar = props => {
             <Logo />
           </Heading>
         </Flex>
+        <Stack
+          direction={{ base: 'column', md: 'row' }}
+          display={{ base: 'none', md: 'flex' }}
+          width={{ base: 'full', md: 'auto' }}
+          alignItems="center"
+          flexGrow={1}
+          mt={{ base: 4, md: 0 }}
+        >
+          <LinkItem href="/works" path={path}>
+            Works
+          </LinkItem>
+          <LinkItem href="/posts" path={path}>
+            Posts
+          </LinkItem>
+        </Stack>
+        <Box flex={1} align="right">
+          <Box ml={2} display={{base:'inline-block', md:'none'}}>
+            <Menu>
+              <MenuButton
+                as={IconButton}
+                icon={<HamburgerIcon />}
+                variant="outline"
+                aria-label="Options"
+              />
+              <MenuList>
+                <NextLink href="/" passHref>
+                  <MenuItem as={Link}>About</MenuItem>
+                </NextLink>
+                <NextLink href="/works" passHref>
+                  <MenuItem as={Link}>Works</MenuItem>
+                </NextLink>
+                <NextLink href="/posts" passHref>
+                  <MenuItem as={Link}>Posts</MenuItem>
+                </NextLink>
+                  <MenuItem as={Link} href="https://github.com/zhijiezh/Personal-Website" target="_blank">View Source</MenuItem>
+              </MenuList>
+            </Menu>
+          </Box>
+        </Box>
       </Container>
     </Box>
   )
