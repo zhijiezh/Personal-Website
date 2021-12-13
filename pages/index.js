ee8fec1a635a6602cd672a4767410852416fcd38
zhijiezh <jerry_shh@hotmail.com>
2021-12-13 18:08:34 -0500

    Add personal profile photo

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,40 @@
-import { Container, Box, Heading } from "@chakra-ui/react"
+import { Container, Box, Heading, Image, useColorModeValue } from '@chakra-ui/react'
 
-const Page = () =>{
+const Page = () => {
   return (
     <Container>
-      <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+      <Box
+        borderRadius="lg"
+        mb={6}
+        p={3}
+        textAlign="center"
+        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+      >
         Hello, I &apos;m a UX developer / engineer
       </Box>
-      <Box display={{md:'flex'}}>
+      <Box display={{ md: 'flex' }}>
         <Box flexGrow={1}>
-          <Heading as="h2" variant = "page-title">
+          <Heading as="h2" variant="page-title">
             Zhijie Zhao
           </Heading>
-          <p> Working hard to become a Developer/ Designer </p>
+          <p> Working hard to become a UX Developer/ Designer </p>
+        </Box>
+        <Box
+          flexShrink={0}
+          mt={{ base: 4, md: 0 }}
+          ml={{ md: 6 }}
+          align="center"
+        >
+          <Image
+            borderColor="whiteAlpha.800"
+            borderWidth={2}
+            borderStyle="solid"
+            maxWidth="100px"
+            display="inline-block"
+            borderRadius="full"
+            src="/images/zhijie.jpg"
+            alt="Profile image"
+          />
         </Box>
       </Box>
     </Container>
