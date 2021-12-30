3da501a360bbe5878e0ae33ecb3112855607989e
zhijiezh <jerry_shh@hotmail.com>
2021-12-29 21:44:59 -0500

    feat: add redlakecounty page

--- pages/works/RedLakeCounty.js
+++ pages/works/RedLakeCounty.js
@@ -1,42 +1,100 @@
-import { Container, Badge, Link, List, ListItem, Box, HStack } from '@chakra-ui/react'
-import { Title, Meta, MobileWorkImage } from '../../components/work'
+import {
+  Container,
+  Badge,
+  Link,
+  List,
+  ListItem,
+  Box,
+  HStack,
+  UnorderedList
+} from '@chakra-ui/react'
+import { Title, Meta, MobileWorkImage, WorkImage } from '../../components/work'
 import Layout from '../../components/layouts/article'
 import Paragraph from '../../components/paragraph'
+import { ExternalLinkIcon } from '@chakra-ui/icons'
+
+const repoLink = 'https://github.com/zhijiezh/SI649-final-proj'
+const webLink =
+  'https://share.streamlit.io/zhijiezh/si649-interactive-communicative-visualization/interactive.py'
+const articleLink =
+  'https://www.washingtonpost.com/news/wonk/wp/2015/09/03/i-called-this-place-americas-worst-place-to-live-then-i-went-there/'
+const staticPdfLink = "../RedLakeCounty-static.pdf"
 
 const Work = () => (
-  <Layout title="HumanKind">
+  <Layout title="The Red Lake County">
     <Container>
       <Title>
-        Wishlist <Badge>2021</Badge>
+        Red Lake County is not perfect but definitely not the worst{' '}
+        <Badge>2021</Badge>
       </Title>
       <Paragraph>
-        placeholder
+        In this data visualization object, I focus on communicative
+        visualization. I designed visualization to accompany the article{' '}
+        <Link href={articleLink} isExternal>
+          I called this place 'Americas worst place to live.' Then I went there.{' '}
+          <ExternalLinkIcon mx="2px" />
+        </Link>{' '}
+        I created both a static data visualization poster and an interactive
+        website. In the project you will see data visualizations for the
+        following topics:
+      </Paragraph>
+      <UnorderedList>
+        <ListItem>Does Red Lake County have good natural amenities?</ListItem>
+        <ListItem>Education and Economy.</ListItem>
+        <ListItem>
+          Red Lake County has done a great job in decreasing the unemployment
+          rate.
+        </ListItem>
+      </UnorderedList>
+      <Paragraph>
+        I also used a parallel coordinates graph to implement a playground where
+        readers can explore the pattern hidden inside the data by themselves.
+        This is also called a Martini Glass Infographics Structure.
       </Paragraph>
       <List ml={4} my={4}>
         <ListItem>
-          <Meta>Solution Overview</Meta>
-          <Paragraph>
-            <a href='https://www.washingtonpost.com/news/wonk/wp/2015/09/03/i-called-this-place-americas-worst-place-to-live-then-i-went-there/' target="_blank">
-              link to the original article
-            </a>
-            placeholder
-          </Paragraph>
+          <Meta>Stack</Meta>
+          <span>Python, Altair, streamlit</span>
         </ListItem>
         <ListItem>
-          <Meta>Stack</Meta>
-          <span>placeholder</span>
+          <Meta>Source</Meta>
+          <Link href={repoLink} isExternal>
+            Github Repo <ExternalLinkIcon mx="2px" />
+          </Link>
+        </ListItem>
+        <ListItem>
+          <Meta>Demo</Meta>
+          <Link href={staticPdfLink} isExternal>
+            The static data visualization <ExternalLinkIcon mx="2px" />
+          </Link>
+        </ListItem>
+        <ListItem>
+          <Meta>Demo</Meta>
+          <Link href={webLink} isExternal>
+            The interactive data visualization <ExternalLinkIcon mx="2px" />
+          </Link>
         </ListItem>
       </List>
-      <HStack alignItems={'center'} justifyContent={'space-around'}>
-          {/* <MobileWorkImage
-            src="/images/works/HumanKind_01.png"
-            alt="HumanKind - Welcome Screen"
-          />
-          <MobileWorkImage
-            src="/images/works/HumanKind_02.png"
-            alt="HumanKind - Home Screen"
-          />  */}
-      </HStack>
+      <WorkImage
+        src="/images/works/RedLakeCounty_static.png"
+        alt="RedLakeCounty - static data vis"
+      />
+      <WorkImage
+        src="/images/works/RedLakeCounty_01.png"
+        alt="RedLakeCounty - Natural Amenities Scale"
+      />
+      <WorkImage
+        src="/images/works/RedLakeCounty_02.png"
+        alt="RedLakeCounty - Education and Economy"
+      />
+      <WorkImage
+        src="/images/works/RedLakeCounty_03.png"
+        alt="RedLakeCounty - Improvement in unemployment"
+      />
+      <WorkImage
+        src="/images/works/RedLakeCounty_04.png"
+        alt="RecipeBook - Patterns hidden inside"
+      />
     </Container>
   </Layout>
 )
