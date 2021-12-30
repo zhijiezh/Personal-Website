6f9c0982f1ea7ceea8e9dc4718fdbcf4345e71ce
zhijiezh <jerry_shh@hotmail.com>
2021-12-29 21:17:47 -0500

    feat: add recipe book page.

--- pages/works/RecipeBook.js
+++ pages/works/RecipeBook.js
@@ -1,39 +1,48 @@
-import { Container, Badge, Link, List, ListItem, Box, HStack } from '@chakra-ui/react'
-import { Title, Meta, MobileWorkImage } from '../../components/work'
+import { Container, Badge, Link, List, ListItem, Box, HStack, UnorderedList } from '@chakra-ui/react'
+import { Title, Meta, MobileWorkImage, WorkImage } from '../../components/work'
 import Layout from '../../components/layouts/article'
 import Paragraph from '../../components/paragraph'
+import { ExternalLinkIcon } from '@chakra-ui/icons'
+
+const repoLink = "https://github.com/zhijiezh/ng-course-recipe-book"
+const webLink = "https://ng-course-recipe-book-1a6c1.web.app/"
 
 const Work = () => (
-  <Layout title="HumanKind">
+  <Layout title="Recipe Book">
     <Container>
       <Title>
-        Wishlist <Badge>2021</Badge>
+        Recipe Book <Badge>2021</Badge>
       </Title>
       <Paragraph>
-        placeholder
+         This is an AngularJS project that implement a recipe book where user can create and save their own recipes. They can also add the ingredients to their shopping carts.
       </Paragraph>
+      <UnorderedList mt={4}>
+        <ListItem>Support login and signup through Firebase.</ListItem>
+        <ListItem>Create, update, delete and edit your own recipes.</ListItem>
+        <ListItem>Add ingredients into your shopping cart.</ListItem>
+        <ListItem>Using NgRx to support data storage.</ListItem>
+      </UnorderedList>
       <List ml={4} my={4}>
         <ListItem>
-          <Meta>Solution Overview</Meta>
-          <Paragraph>
-            placeholder
-          </Paragraph>
+          <Meta>Stack</Meta>
+          <span>Angular, Firebase</span>
         </ListItem>
         <ListItem>
-          <Meta>Stack</Meta>
-          <span>placeholder</span>
+          <Meta>Source</Meta>
+          <Link href={repoLink} isExternal>
+            Github Repo <ExternalLinkIcon mx="2px" />
+          </Link>
+        </ListItem>
+        <ListItem>
+          <Meta>Demo</Meta>
+          <Link href={webLink} isExternal>
+           The recipe book website <ExternalLinkIcon mx="2px" />
+          </Link>
         </ListItem>
       </List>
-      <HStack alignItems={'center'} justifyContent={'space-around'}>
-          {/* <MobileWorkImage
-            src="/images/works/HumanKind_01.png"
-            alt="HumanKind - Welcome Screen"
-          />
-          <MobileWorkImage
-            src="/images/works/HumanKind_02.png"
-            alt="HumanKind - Home Screen"
-          />  */}
-      </HStack>
+      <WorkImage src="/images/works/RecipeBook_03.png" alt="RecipeBook - signup"/>
+      <WorkImage src="/images/works/RecipeBook_01.png" alt="RecipeBook - Recipe List"/>
+      <WorkImage src="/images/works/RecipeBook_02.png" alt="RecipeBook - Shopping Cart"/>
     </Container>
   </Layout>
 )
