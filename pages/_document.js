13386e38ecf870cc3342e1286bd802b2ca0685e4
zhijiezh <jerry_shh@hotmail.com>
2021-12-30 15:04:03 -0500

    feat: add favicon

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,35 @@
-import { ColorModeScript } from "@chakra-ui/react";
+import { ColorModeScript } from '@chakra-ui/react'
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
 
 import theme from '../lib/theme'
 
-export default class Document extends NextDocument{
-  render(){
-    return(
-      <Html lang='en'>
-        <Head />
+export default class Document extends NextDocument {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#eee3d6" />
+          <meta name="msapplication-TileColor" content="#eee3d6" />
+          <meta name="theme-color" content="#ffffff" />
+        </Head>
         <body>
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Main />
